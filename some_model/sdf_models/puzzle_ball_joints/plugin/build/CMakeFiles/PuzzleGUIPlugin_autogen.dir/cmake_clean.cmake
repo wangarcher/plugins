7eafef7464d8d4f2b2da5fecf126a4104432e59c
wangarcher <747867532@qq.com>
2023-03-22 12:12:39 +0800

    remove

--- some_model/sdf_models/puzzle_ball_joints/plugin/build/CMakeFiles/PuzzleGUIPlugin_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PuzzleGUIPlugin_autogen"
-  "PuzzleGUIPlugin_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PuzzleGUIPlugin_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
